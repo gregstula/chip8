8dced5e85238f9748589fd140587f21d84882ced
Greg Stula <gregstula@me.com>
2020-12-30 21:18:42 -0500

    Add try block to main

--- main.cpp
+++ main.cpp
@@ -1,38 +1,44 @@
 #include "chip8.hpp"
 #include "display.hpp"
+#include <exception>
 #include <iostream>
 
 int main()
 {
-    chip8::display display {};
-    chip8::vm vm {};
+    try {
+        chip8::display display {};
+        chip8::vm vm {};
 
-    vm.load_rom("ibmlogo.ch8");
-    vm.start_timers();
+        vm.load_rom("ibmlogo.ch8");
+        vm.start_timers();
 
-    bool quit = false;
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
+        bool quit = false;
+        while (!quit) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
 
-            if (event.type == SDL_QUIT) {
-                quit = true;
-                break;
+                if (event.type == SDL_QUIT) {
+                    quit = true;
+                    break;
+                }
+                else if (event.type == SDL_KEYDOWN) {
+                }
+                else if (event.type == SDL_KEYUP) {
+                }
             }
-            else if (event.type == SDL_KEYDOWN) {
-            }
-            else if (event.type == SDL_KEYUP) {
-            }
-        }
-        vm.tick();
-        display.render(vm.screen);
-        /*            int count = 0;
+            vm.tick();
+            display.render(vm.screen);
+            /*            int count = 0;
             for (auto&& i : vm.screen) {
                 std::cout << (int)i;
                 if (++count % chip8::SCREEN_WIDTH == 0) std::cout << "\n";
             }
             std::cout << "OP:" << std::hex << (int)vm.current_op.type << std::endl;
 */
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
     return 0;
 }
